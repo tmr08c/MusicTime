51f0edc4a21f3c6fe4ce0db7b1e567e46294ee42
Troy Rosenberg <tmr08c@gmail.com>
2015-03-01 19:11:08 -0500

    Creates proof of concept front end.
    * Restructures directory to follow more common examples
    * Creates a basic index page that uses JS to make an AJAX call to our Sinatra backend.

--- Gemfile
+++ Gemfile
@@ -4,8 +4,10 @@ gem 'sinatra'
 gem 'json'
 gem 'lastfm'
 gem 'figaro'
+gem 'slim', '~> 3.0.2'
 
 group :test, :development do
   gem 'rspec'
   gem 'rack-test'
+  gem 'debugger'
 end
--- application.rb
+++ application.rb
@@ -1,13 +1,21 @@
 require 'sinatra'
 require 'json'
-require_relative 'app/models/last_fm_api'
-require_relative 'app/models/track_collector'
-require_relative 'app/models/track_processor'
-require_relative 'app/models/track_processor'
+require 'slim'
+require 'debugger'
+require_relative 'models/last_fm_api'
+require_relative 'models/track_collector'
+require_relative 'models/track_processor'
+require_relative 'models/track_processor'
+
+
+get '/' do
+  @title = 'Music Time'
+  slim :index
+end
 
 get '/api/v1/recent_tracks', provides: 'json' do
   if params['username'] && params['count']
-    TrackCollector.new(params['username']).recent_tracks(Integer(params['count'])).to_json
+    TrackCollector.new(params['username']).recent_tracks(Integer(params['count'])).map(&:to_h).to_json
   else
     'Handle Me!'
   end
